988ea642dc127b158c2e73941280347ed474f90e
charredlot <chatapp@instance-1.c.mystical-hawk-771.internal>
2014-11-30 19:43:35 +0000

    move some settings around, cosmetic

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+class Settings(object):
+    GAUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+    GAUTH_TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+    GAUTH_SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
+    GAUTH_PROFILE_URI = 'https://www.googleapis.com/oauth2/v1/userinfo'
+    GAUTH_SECRET = 'OW-ylwg1IgZH9GgDSc9pwp_l'
+    GAUTH_CLIENT_ID = '934594949893-f914vohd4d45bs6ihmlu7s4c6eh6ee70.apps.googleusercontent.com'
+    GAUTH_TOKEN_REVOKE_URI = 'https://accounts.google.com/o/oauth2/revoke'
+    EXTERNAL_ADDRESS = 'http://146.148.81.187.xip.io:5000'
+
